67c41b1668c4586b876663c5a1aa73a67a13ec50
LyciaMessaoui <messaouilycia@gmail.com>
2020-02-04 12:32:35 +0100

    v1.1 front

---
+++ react-frontend/src/Users.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './assets/style.css';
+import axios from 'axios';
+class Users extends Component {
+ 
+ constructor(props){
+ super(props);
+ this.state = {
+ posts:[]
+ }
+ }
+ componentDidMount(){
+ axios.get('http://localhost:6200/users')
+ .then(response =>{
+ console.log(response)
+ this.setState({posts: response.data})
+ })
+ .catch(error=>{
+ console.log(error)
+ })
+ }
+ render(){
+ const { posts}=this.state
+ return(
+ <div>List Of Users 
+ {
+ posts.length ?
+ posts.map(post=><div key={post.id}><p>{post.username} : </p> {post.email}</div>):
+ null
+ }
+ </div>
+ )
+ }
+}
+export default Users
