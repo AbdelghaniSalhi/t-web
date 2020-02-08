68b56291cdb25685a25e81c890ebdc0ec7479a75
sara <sara.bourahla1997@gmail.com>
2020-02-08 18:47:47 +0100

    Font v1.3

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login  from '../src/Login';
 import  Register from '../src/Register';
 import PrincipalOff2 from './PrincipalOff2';
-import PrincipalOff from './PrincipalOff';
 
+import PrinOn from './PrinOn';
+
+import Admin from './Admin';
 
 
 class App extends Component {
@@ -14,11 +16,11 @@ class App extends Component {
         <Router>
          
             <Switch>
-            <Route exact path="/" component={PrincipalOff} />
+            <Route exact path="/" component={PrinOn} />
               <Route  path="/PrincipalOff2" component={PrincipalOff2} />
               <Route path="/Register" component={Register} />
               <Route path="/Login" component={Login} />
-              
+              <Route path="/Admin" component={Admin} />
             </Switch>
          
         </Router>
