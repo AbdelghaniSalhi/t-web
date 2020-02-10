76bfd708233f9bafd4d9e8185d12506b1e25a1fa
AbdelghaniSalhi <abdelghani.salhi@epitech.eu>
2020-02-10 18:52:36 +0100

    ajout des articles

--- node-backend/src/routes/articles.js
+++ node-backend/src/routes/articles.js
@@ -1,20 +1,34 @@
 const router = require('express').Router();
-let Article = require('../models/article.models');
 const jwt = require('jsonwebtoken');
 const verifié = require ('./verifierToken');
+const axios = require('axios');
+var CryptoNewsAPI = require('crypto-news-api');
+let CryptoCurrency = require ('../models/crypto.models');
 
 // Get all Articles
 router.route('/').get((req, res) =>{
-    Article.find()
-        .then(articles => res.json(articles))
+    
+    axios.get("https://cryptocontrol.io/api/v1/public/news/", {headers :{ "x-api-key": process.env.API_ARTICLES}})
+        .then(response => {
+            let result = [];
+            console.log(response.data)
+            let mots = []
+            for (let i=0; i<response.data.length; i++){
+                let elem = {
+                    id : response.data[i]._id,
+                    coins : response.data[i].coins,
+                    summary: response.data[i].description,
+                    url : response.data[i].url,
+                    image : response.data[i].thumbnail,
+                    source: response.data[i].source,
+                }
+                result.push(elem);
+            }
+            res.send(result);
+        })    
         .catch(err => res.status(400).json('Error : ' + err));
 });
 
-// Get By Id Secure Route
-router.route('/:articleId').get(verifié, (req, res) =>{
-    const id = req.params.articleId;
 
-    Article.findById(id)
-        .then(article => res.json(article))
-        .catch(err => res.status(400).json('Error : ' + err));
-});
+
+module.exports = router; 
